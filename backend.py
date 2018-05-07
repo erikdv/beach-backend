5c0ad1653e40a40569854be55c6328726f9cda5c
Erik de Vries <erik@edia.nl>
2018-05-07 22:30:08 +0200

    Cleaned up the code: name of the beach now part of the GET request

--- backend.py
+++ backend.py
@@ -16,36 +16,31 @@ mongo = PyMongo(app)
 
 @app.route('/beach', methods=['GET'])
 def get_all_beachs():
-  beach = mongo.db.beachs
-  s = beach.find_one({})
-  if s:
-    output = {'name' : s['name'], 'rating' : s['rating']}
-  else:
-    output = "No such name"
-#  output = []
-#  for s in beach.find():
-#    output.append({'name' : s['name'], 'rating' : s['rating']})
-  return jsonify({"type": "success", 'value' : output})
+    beach = mongo.db.beachs
+    output = []
+    for s in beach.find():
+        output.append({'name' : s['name'], 'rating' : s['rating']})
+    return jsonify({"type": "success", 'value' : output})
 
-@app.route('/beach/', methods=['GET'])
+@app.route('/beach/<name>', methods=['GET'])
 def get_one_beach(name):
-  beach = mongo.db.beachs
-  s = beach.find_one({'name' : name})
-  if s:
-    output = {'name' : s['name'], 'rating' : s['rating']}
-  else:
-    output = "No such name"
-  return jsonify({'result' : output})
+    beach = mongo.db.beachs
+    s = beach.find_one({'name' : name})
+    if s:
+        output = {'name' : s['name'], 'rating' : s['rating']}
+    else:
+        output = "No such name"
+    return jsonify({"type": "success", 'value' : output})
 
 @app.route('/beach', methods=['POST'])
 def add_beach():
-  beach = mongo.db.beachs
-  name = request.json['name']
-  rating = request.json['rating']
-  beach_id = beach.insert({'name': name, 'rating': rating})
-  new_beach = beach.find_one({'_id': beach_id })
-  output = {'name' : new_beach['name'], 'rating' : new_beach['rating']}
-  return jsonify({'result' : output})
+    beach = mongo.db.beachs
+    name = request.json['name']
+    rating = request.json['rating']
+    beach_id = beach.insert({'name': name, 'rating': rating})
+    new_beach = beach.find_one({'_id': beach_id })
+    output = {'name' : new_beach['name'], 'rating' : new_beach['rating']}
+    return jsonify({'result' : output})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
